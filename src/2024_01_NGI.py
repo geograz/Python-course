78ced1660bf759f33146b3350900ad3a72c9f22e
G. H. Erharter <32653899+geograz@users.noreply.github.com>
2024-05-06 10:37:38 +0200

    Create 2024_01_NGI.py

---
+++ src/2024_01_NGI.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Script for the "Python basics for geoscience and geotechnics (Pilot course)"
+from the Norwegian Geotechnical Institute. The course is held in May 2024 in
+4x4 hour sessions.
+
+This script contains the code that was written during the sessions for
+educational purposes only.
+All content of the repository falls under the MIT-license -> see license file.
+
+Author: Dr. Georg H. Erharter, georg.erharter@ngi.no
+"""
+
+###########################
+# session 1 on 6th May 2024
+###########################
+
+### introduction and overview over Spyder
+
+
+### datatypes: integers, floats, strings, print function
+
+# Exercise 1
+
+
+### operators: +, -, /, *, **, //, %
+
+# Exercise 2
+
+
+### datatypes: lists, tuples, dictionaries, indices, exceptions / errors
+
+# Exercise 3
+
+# Exercise 4
+
+
+### conditional statements: if, elif, else, match cases
+
+
+### loops: while loop, for loop
+
+# Exercise 5
+
+# Exercise 6
+
+# Exercise 7
+
+# Exercise 8
+
+# Exercise 9
+
+### functions
+
+# Exercise 10
+
+
+### modules, code environments, data visualization
+
+# Exercise 11
+
+### data handling with pandas
+# import CPT dataset Premstaller and visualize random CPT test
+
+# Exercise 12
+
+# Exercise 13
+
+# Exercise 14
+
