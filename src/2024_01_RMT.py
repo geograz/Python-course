e30df0a438ca63ed511af2dfdd87a38975c95060
G. H. Erharter <32653899+geograz@users.noreply.github.com>
2024-05-22 15:24:24 +0200

    Create 2024_01_RMT.py

---
+++ src/2024_01_RMT.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+This script contains the code that was written during the first session for
+educational purposes only.
+All content of the repository falls under the MIT-license -> see license file.
+
+Author: Dr. Georg H. Erharter, georg.erharter@ngi.no
+"""
+
+###########################
+# session 1 on 27th May 2024
+###########################
+
+### introduction and overview over Spyder
+
+
+### datatypes: integers, floats, strings, print function
+
+# Exercise 1
+
+
+### operators: +, -, /, *, **, //, %
+
+# Exercise 2
+
+
+### datatypes: lists, tuples, dictionaries, indices, exceptions / errors
+
+# Exercise 3
+
+# Exercise 4
+
+
+### conditional statements: if, elif, else, match cases
+
+
+### loops: while loop, for loop
+
+# Exercise 5
+
+# Exercise 6
+
+# Exercise 7
+
+# Exercise 8
+
+# Exercise 9
+
+### functions
+
+# Exercise 10
+
+
+### modules, code environments, data visualization
+
+# Exercise 11
+
+### data handling with pandas
+# import CPT dataset Premstaller and visualize random CPT test
+
+# Exercise 12
+
+# Exercise 13
+
+# Exercise 14
+
